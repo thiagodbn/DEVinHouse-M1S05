1889dd75f2dfde28a841cd2e41e913ce66f57385
Thiago D B Noronha <thiagodbn@gmail.com>
2023-06-13 14:30:18 -0300

    EX3 M1S05

--- exercicio3.js
+++ exercicio3.js
@@ -5,3 +5,29 @@
  * é um número primo. Um número primo é aquele que é divisível apenas por 1 e por ele mesmo.
  * Utiliza uma estrutura de repetição (do while) para verificar as divisões.
  */
+
+
+var numero = 12;
+var divisor = 2;
+
+  if (numero <= 1) {
+     console.log('Numero não é primo')
+  } else  {
+  
+  while (divisor <= numero) {
+    if (divisor === numero && numero % divisor === 0) {
+        console.log(numero + ' é um numero primo ');
+        break;
+        
+      }else if(numero % divisor === 0){
+        console.log(numero + ' náo é um numero primo ');
+        break
+      } 
+       else {
+        divisor++;
+      }
+
+};}
+
+
+
