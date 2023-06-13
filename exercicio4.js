aa4012386b4846933f29d804cd9c35c5cc1a5235
Thiago D B Noronha <thiagodbn@gmail.com>
2023-06-13 15:32:06 -0300

    EX4 M1S05

--- exercicio4.js
+++ exercicio4.js
@@ -6,3 +6,29 @@
  * 0, 1, 1, 2, 3, 5, 8, 13, ...
  * Utiliza uma estrutura de repetição (while) para gerar a sequência.
  */
+
+var numero = 13 // numero que dira quantas casas rodara 
+var fibo = 0
+var thi = 0
+var n = 0
+while (fibo <= numero) {
+    console.log(fibo)
+if (numero === fibo){
+    console.log ('Finaliza')
+break;
+} else if (fibo === 0 ){
+    fibo++;
+} else if (fibo === 1){
+    console.log(fibo)
+    var thi = fibo
+    fibo++;
+}
+else {
+    fibo = thi + fibo - n ;
+    var thi = fibo
+    n++
+}
+
+
+
+}
