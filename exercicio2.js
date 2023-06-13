b9322186cbfae09fd0f448c66693565130589f53
Thiago D B Noronha <thiagodbn@gmail.com>
2023-06-13 11:23:33 -0300

    EX2 M1S05

--- exercicio2.js
+++ exercicio2.js
@@ -5,3 +5,28 @@
  * a tradução para a mesma cor em português. Utiliza uma estrutura condicional (switch)
  * para fazer o mapeamento das cores.
  */
+
+var cor = 'WhiTe' 
+
+switch (cor.toLowerCase()){
+    case 'red': console.log('Vermelho');
+    break;
+    case 'blue': console.log('Azul');
+    break;
+    case 'green': console.log('Verde');
+    break;
+    case 'yellow': console.log('Amarelo');
+    break;
+    case 'pink': console.log('Rosa');
+    break;
+    case 'purple': console.log('Roxo');
+    break;
+    case 'brown': console.log('Marrom');
+    break;
+    case 'Orange': console.log('Laranja');
+    break;
+    case 'white': console.log('Branco');
+    break;
+    
+    default:    console.log(`Desculpe, ${cor} não é uma cor valida.`);
+}
