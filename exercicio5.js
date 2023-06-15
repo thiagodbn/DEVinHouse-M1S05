b235ff7b654cdf17b90d8f13ea26ac06476efa89
Thiago D B Noronha <thiagodbn@gmail.com>
2023-06-15 18:22:09 -0300

    EX5 M1S05 resolvido

--- exercicio5.js
+++ exercicio5.js
@@ -4,3 +4,21 @@
  * Escreve um programa que percorre um array de palavras e exibe todas as palavras que possuem
  * mais de 5 caracteres. Utiliza a estrutura de repetição for of para percorrer o array.
  */
+
+
+let palavras = ['Amar', 'Sol', 'Mar', 'Cão', 'Bola', 'Livro',
+ 'Amigo', 'Carro', 'Chave', 'Pão', 'Arroz', 'Faca', 'Tesoura',
+'Avião', 'Navio', 'Praia', 'Pessoa', 'Colher', 'Elefante', 'Cavalo',
+'Piano', 'Computador', 'Travesseiro', 'Universo',]
+
+let palavrasUp = []
+
+var caracteres = 5  //caracteres ou mais
+
+for (let palavra of palavras) {
+    if(palavra.length>=caracteres){
+        palavrasUp.push(palavra)
+    }
+  }
+  console.log(`Palavras com mais de ${caracteres} letras`)
+  console.log(palavrasUp);
