ee214ada9ed0412d19ed9b2f6b8d8235ad37432a
Thiago D B Noronha <thiagodbn@gmail.com>
2023-06-16 11:31:28 -0300

    EX6 M1S05 Resolvido

--- exercicio6.js
+++ exercicio6.js
@@ -5,3 +5,55 @@
  * e o preço de cada produto. Após imprimir essas informações, imprime o total de produtos
  * e o valor total de todos os produtos.
  */
+
+let produtos = [
+    { nome: 'Paracetamol', preco: 8.50, quantidade: 5 },
+    { nome: 'Dipirona', preco: 6.50, quantidade: 3 },
+    { nome: 'Omeprazol', preco: 15.80, quantidade: 7 },
+    { nome: 'Amoxicilina', preco: 12.10, quantidade: 2 },
+    { nome: 'Ibuprofeno', preco: 10.50, quantidade: 4 },
+    { nome: 'Dorflex', preco: 9.50, quantidade: 6 },
+    { nome: 'Ranitidina', preco: 7.30, quantidade: 1 },
+    { nome: 'Dexametasona', preco: 18.50, quantidade: 8 },
+    { nome: 'Fluoxetina', preco: 14.20, quantidade: 9 },
+    { nome: 'Atenolol', preco: 11.60, quantidade: 10 }
+]
+
+let nomeDesejado = 'Paracetamol'; // Nome do produto desejado
+let produtoEncontrado = null; // Variável para armazenar o objeto encontrado
+
+// Percorre o array de produtos
+for (let i = 0; i < produtos.length; i++) {
+  if (produtos[i].nome === nomeDesejado) {
+    produtoEncontrado = produtos[i]; // Armazena o objeto encontrado
+    break; // Interrompe o loop assim que encontrar o objeto desejado
+  }
+}
+
+// Verifica se o objeto foi encontrado
+if (produtoEncontrado) {
+    console.log(nomeDesejado)
+  console.log(`Quantidade: ${produtoEncontrado.quantidade}`);  
+  console.log(`O Preço é R$ ${produtoEncontrado.preco}`);
+  
+} else {
+  console.log('Produto não encontrado.');
+}
+
+// Percorre o array de produtos e verifica a quantidade total disponivel
+let qntTotal = 0
+
+for (let i = 0; i < produtos.length; i++) {
+    qntTotal += produtos[i].quantidade
+  }
+
+  console.log(`Total de produtos disponiveis ${qntTotal}`)
+
+ // Percorre o array de produtos e verifica total preço (somatorio quantidade * Preço)
+let qntPreco = 0
+
+for (let i = 0; i < produtos.length; i++) {
+    qntPreco += produtos[i].quantidade * produtos[i].preco;
+  }
+
+  console.log(`Tem R$ ${qntPreco.toFixed(2)} em produtos`)
