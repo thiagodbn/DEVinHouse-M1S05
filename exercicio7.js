66f35ac05c1014ed3ac08ca054e439772a5ae364
Thiago D B Noronha <thiagodbn@gmail.com>
2023-06-18 17:58:40 -0300

    EX7 M1S05 inciio resolução

--- exercicio7.js
+++ exercicio7.js
@@ -10,3 +10,19 @@
  *      remover os elementos do array compreendidos entre os índices inicial e final. Em seguida, retorna o
  *      array atualizado.
  */
+
+
+//             0  1  2  3  4  5  6  7  8  9   10
+let valores = [1, 2, 3, 4, 5, 6, 7, 8 ,9 ,10 ,11]
+
+valores.push(12, 13)  // adiciona ao final do array
+
+console.log(valores) 
+
+valores.pop() // remove ultimo elemento
+
+console.log(valores)
+
+ 
+
+console.log(valores.slice(3,8) )//slice([início[,fim]])
