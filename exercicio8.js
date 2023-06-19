b5848e275dbd54080265ee21ab2093448ec21089
Thiago D B Noronha <thiagodbn@gmail.com>
2023-06-19 11:34:29 -0300

    EX8 M1S05 resolvido

--- exercicio8.js
+++ exercicio8.js
@@ -5,3 +5,25 @@
  * matemática. A função `calculadora` executa a operação matemática nos dois números fornecidos e retorna o resultado.
  * Testa a função utilizando diferentes operações como adição, subtração, multiplicação e divisão.
  */
+
+function Calculadora(num1, num2, operador){
+    if (operador === '+' ){
+        result = num1 + num2
+    } else if (operador === '-') {
+        result = num1 - num2
+    } else if (operador === '*') {
+        result = num1 * num2
+    } else if (operador === '/') {
+        result = num1 / num2
+    }else  {
+        result = 'Invalido'
+    }
+    
+    console.log(result)
+}
+
+Calculadora(1,3,'+')
+Calculadora(10,6,'-')
+Calculadora(2,2,'*')
+Calculadora(8,2,'/')
+Calculadora(1,2,'P')
